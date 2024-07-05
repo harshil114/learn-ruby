d4de3a8d4a821a877e3e73272cedcbfb1ece29f4
harshil114 <harshilvispute114@gmail.com>
2024-07-05 14:00:58 +0530

    Add if_else_if example

---
+++ conditional-statements/if_else_if.rb
@@ -0,0 +1,12 @@
+a = 40
+if a < 33
+    puts "Fail"
+elsif a >= 33 && a <= 50
+    puts "D Grade"
+elsif a >= 50 && a <= 65
+    puts "C Grade"
+elsif a >= 65 && a <= 80 
+    puts "B Grade"
+elsif a >=80 && a <= 100
+    puts "A Grade"
+end

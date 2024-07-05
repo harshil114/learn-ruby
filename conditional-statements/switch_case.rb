95d570f39a33a5968d28d03f9fd684ebd799408b
harshil114 <harshilvispute114@gmail.com>
2024-07-05 14:07:07 +0530

    Add switch_case example

---
+++ conditional-statements/switch_case.rb
@@ -0,0 +1,14 @@
+print "Input numbr from 1,2,3,4: "
+str = gets.chomp 
+case str
+when "1"
+    puts "Input is 1"
+when "2"
+    puts "Input is 2"
+when "3"
+    puts "Input is 3"
+when "4"
+    puts "Input is 4"
+else
+    puts "Invalid Number!"
+end

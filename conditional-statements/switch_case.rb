3a366dd8673a410e8fa9a42e6c0a79a17e05af94
harshil114 <harshilvispute114@gmail.com>
2024-07-05 14:13:16 +0530

    Add switch_case with different syntax

--- conditional-statements/switch_case.rb
+++ conditional-statements/switch_case.rb
@@ -1,14 +1,32 @@
-print "Input numbr from 1,2,3,4: "
-str = gets.chomp 
-case str
-when "1"
-    puts "Input is 1"
-when "2"
-    puts "Input is 2"
-when "3"
-    puts "Input is 3"
-when "4"
-    puts "Input is 4"
+# print "Input numbr from 1,2,3,4: "
+# str = gets.chomp 
+# case str
+# when "1"
+#     puts "Input is 1"
+# when "2"
+#     puts "Input is 2"
+# when "3"
+#     puts "Input is 3"
+# when "4"
+#     puts "Input is 4"
+# else
+#     puts "Invalid Number!"
+# end
+
+print "Add marks: "
+
+marks = gets.chomp.to_i 
+
+case marks
+
+when 0..32
+    puts "Fail"
+when 33..40
+    puts "C Grade"
+when 41..60
+    puts "B Grade"
+when 60..80
+    puts "A Grade"
 else
-    puts "Invalid Number!"
+    puts "A+ Grade"
 end
